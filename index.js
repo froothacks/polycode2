56727ec098318106b3381c08e76e5c2006602c05
Advait-M <advait.maybhate9@gmail.com>
2018-09-29 16:04:37 -0400

    Adding Translate API functionality

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
 const program = require('commander');
-const translator = require('./translate.js')
+const translator = require('./translate.js');
 
 program
   .arguments('command')
   // .option('-b, --banana <banana>', 'A banana') would be accessed by program.banana
   .action(command => {
     console.log(`Hi <3 ! The command was ${command}`);
-    translator.test(); 
+    translator.test();
+    translator.translateText('apple', 'en', 'fr')
+        .then(function(value) {
+            console.log(value);
+        });
   })
   .parse(process.argv);
 
--- translate.js
+++ translate.js
@@ -1,6 +1,15 @@
-
-
+var request = require('request');
+function translateText(sourceText, sourceLang, targetLang) {
+    var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl="
+        + sourceLang + "&tl=" + targetLang + "&dt=t&q=" + encodeURI(sourceText);
+    return new Promise(function(resolve, reject) {
+        request(url, function (error, response, body) {
+            resolve(JSON.parse(body)[0][0][0]);
+            // TODO: Error handling
+        });
+    });
+}
 
 module.exports = {
-   test() {console.log("this is the test function for you david")},
+   test() {console.log("this is the test function for you david")}, translateText
 }
