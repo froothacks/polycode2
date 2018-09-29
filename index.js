278824a6319076f754b64ca965257ebdbffe3f48
kipply <carolyfisher@gmail.com>
2018-09-29 15:59:03 -0400

    fix indentation

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
 #!/usr/bin/env node
 const program = require('commander');
 const translator = require('./translate.js')
+const dictionary = require('./dictionary.js')
 
 program
-  .arguments('command')
-  // .option('-b, --banana <banana>', 'A banana') would be accessed by program.banana
-  .action(command => {
-    console.log(`Hi <3 ! The command was ${command}`);
-    translator.test(); 
-  })
-  .parse(process.argv);
-
-function fake_backend(payload) {
-	
-}
-
-function translate_file(config, target_file, SOURCE_LANG, DEST_LANG) {
-
-}
-
-function translate_all(config, DEST_LANG, additional_ignores=[]) {
-
-}
+	.arguments('command')
+	// .option('-b, --banana <banana>', 'A banana') would be accessed by program.banana
+	.action(command => {
+		console.log(`Hi <3 ! The command was ${command}`);
+		translator.test(); 
+	})
+	.parse(process.argv);
--- translate.js
+++ translate.js
@@ -1,6 +1,3 @@
-
-
-
 module.exports = {
    test() {console.log("this is the test function for you david")},
 }
